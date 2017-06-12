78149d06f0f3549dbaf23812405571eb2663ee41
Admin <admin@Mac-Admin.local>
2017-06-12 13:10:27 +0300

    Initial Commit

---
+++ ATMBankoMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ATMBankoMap
+//
+//  Created by Admin on 12.06.17.
+//  Copyright © 2017 ilya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ATMBankoMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ATMBankoMap
+//
+//  Created by Admin on 12.06.17.
+//  Copyright © 2017 ilya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
